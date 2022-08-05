f6d9fb6b922177f15e5b228f960e8b9ac4b7eeef
jeova paiva <jeovappaiva@gmail.com>
2022-08-04 22:12:26 -0300

    exercícios da a012

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let idade = 5;
+
+if (idade >= 5 && idade <= 10){
+    console.log("Aluno aprovado");
+}
+else if (idade >= 3 && idade <= 4){
+   console.log("Aluno recuperação!");
+}
+else if ( idade >= 0 && idade <= 2){
+    console.log("Aluno reprovado!");
+}
+else {
+    console.log("Nota inválida!");
+}
